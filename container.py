4b0efa3a2972cfaeb70a84de969b4ac1e8acaa06
Richard Scott <bububooo3@gmail.com>
2025-05-10 21:26:34 -0400

    start container.py

--- container.py
+++ container.py
@@ -1,7 +1,13 @@
 import commands2
+from wpilib import DriverStation
+
 from constants import *
 from subsystems import arm
 from pint import UnitRegistry
-
 u = UnitRegistry()
+
+class RobotContainer:
+    def __init__(self):
+        DriverStation.silenceJoystickConnectionWarning(True)
+
 
